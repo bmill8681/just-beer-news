eed4c190ae27695e179474ef89d5681bd82b54a7
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-03-06 09:12:02 -0700

    minor changes pre-refactoring

--- src/Components/Home/ArticleList.js
+++ src/Components/Home/ArticleList.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 // Custom Components
 import ArticleItem from './ArticleItem';
-// Styling
 
 /*  articles : Array isRequired 
  *  Displays a list of articles.
@@ -18,9 +17,9 @@ const ArticleList = props => {
                     return (
                         // I would like to assume the article id is unique, but that isn't verified at the moment. So, I'm using the index
                         // as the key for the elements within the array.
-                        // Also - if I was handling CSS differently I could omit the div being used as a divider between articles.
+                        // Also - if I was handling CSS differently I could omit the div being used as a divider between articles and use a pseudo selector.
                         <section key={index}>
-                            <ArticleItem 
+                            <ArticleItem
                                 data={cur}
                             />
                             {index < props.articles.length - 1 ? <div style={{ outline: "1px solid lightgrey" }}></div> : undefined}</section>
