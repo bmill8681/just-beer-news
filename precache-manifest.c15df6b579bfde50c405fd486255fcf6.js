e36203f7ef6a437106d5dfa85b995d8df65c393a
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-03-06 09:43:36 -0700

    Updates

---
+++ precache-manifest.c15df6b579bfde50c405fd486255fcf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aefb28aa7980f10ebd11b441ac33b9b",
+    "url": "/just-beer-news/index.html"
+  },
+  {
+    "revision": "9498b1389f9b746839e2",
+    "url": "/just-beer-news/static/css/main.fd169c1c.chunk.css"
+  },
+  {
+    "revision": "1ce95ba48b82db97f317",
+    "url": "/just-beer-news/static/js/2.3b096296.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/just-beer-news/static/js/2.3b096296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9498b1389f9b746839e2",
+    "url": "/just-beer-news/static/js/main.7a786be8.chunk.js"
+  },
+  {
+    "revision": "854eab47eb9e4e75c96c",
+    "url": "/just-beer-news/static/js/runtime-main.d85276a5.js"
+  }
+]);
